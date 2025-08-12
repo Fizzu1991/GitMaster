1cf23e8e784123b90aa141a1c6ec1db82d3dc4b0
Iam-shafi <fizzushafi@gmail.com>
2025-08-12 18:02:47 +0000

    Remove almightfile.txt and script1.sh

--- script1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "===== Script 1 Started ====="
-sudo apt update -y || sudo yum update -y
-echo "This is script 1 running on EC2"
-# Simulate task
-sleep 3
-echo "===== Script 1 Finished ====="
